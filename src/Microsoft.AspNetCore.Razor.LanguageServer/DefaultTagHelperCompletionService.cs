bf299071c423c2e794b57fbc0d7462d9b08300e1
N. Taylor Mullen <nimullen@microsoft.com>
2019-02-22 11:27:41 -0800

    Add TagHelper attribute completion descriptions.
    - Added tests to ensure the completion descriptions populate correctly. Did only a few integration level tests for the completion description to make things easier to maintain. Didn't want to add a ton of work every time we decided to change what the completion descriptions looked like.
    - Expanded the completion item extensions to allow setting/retrieving of attribute descriptions
    - Had to add a little hack to extract an attributes parent TagHelper type name by looking at its display name. There isn't another way around this given the current set of APIs exposed by core Razor.
    
    #18

--- src/Microsoft.AspNetCore.Razor.LanguageServer/DefaultTagHelperCompletionService.cs
+++ src/Microsoft.AspNetCore.Razor.LanguageServer/DefaultTagHelperCompletionService.cs
@@ -261,6 +261,13 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer
                     Kind = CompletionItemKind.TypeParameter,
                     CommitCharacters = AttributeCommitCharacters,
                 };
+                var attributeDescriptions = completion.Value.Select(boundAttribute => new TagHelperAttributeDescriptionInfo(
+                    boundAttribute.DisplayName,
+                    boundAttribute.GetPropertyName(),
+                    indexerCompletion ? boundAttribute.IndexerTypeName : boundAttribute.TypeName,
+                    boundAttribute.Documentation));
+                var attributeDescriptionInfo = new AttributeDescriptionInfo(attributeDescriptions.ToList());
+                razorCompletionItem.SetDescriptionInfo(attributeDescriptionInfo);
 
                 completionItems.Add(razorCompletionItem);
             }
@@ -301,7 +308,7 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer
                 };
                 var tagHelperDescriptions = completion.Value.Select(tagHelper => new TagHelperDescriptionInfo(tagHelper.GetTypeName(), tagHelper.Documentation));
                 var elementDescription = new ElementDescriptionInfo(tagHelperDescriptions.ToList());
-                razorCompletionItem.SetDescriptionData(elementDescription);
+                razorCompletionItem.SetDescriptionInfo(elementDescription);
 
                 completionItems.Add(razorCompletionItem);
             }
